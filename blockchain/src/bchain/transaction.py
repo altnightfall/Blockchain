7a819e121c38aef0f986badc3c177ee4a292da93
Strelchenko Aleksandr <telikinez@gmail.com>
2023-11-05 20:15:10 +0300

    Finish first draft of transactions

--- blockchain/src/bchain/transaction.py
+++ blockchain/src/bchain/transaction.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from ellipticcurve import PrivateKey, PublicKey, Ecdsa, Signature
 from pickle import dumps, loads
 import type_enforced
+import base64
 
 class TTypes(Enum):
     transfer = 1
@@ -26,6 +27,9 @@ class Address():
 
     def __call__(self) -> str:
         return self.address
+
+    def getAddr(self) -> str:
+        return self.address
     
     @staticmethod
     def validate(address: str) -> bool:
@@ -36,41 +40,63 @@ class Address():
 
 @type_enforced.Enforcer
 class Transaction():
-    __slots__ = ['data', 'dataString', 'signature']
+    __slots__ = ['data', '_datastring', '_signature']
 
-    def __init__(self, ttype : TTypes, fromAddr : Address, toAddr : Address, pkey : PublicKey, ckey : PrivateKey, value : int, fee : int, msg : [None, object] = None) -> None:
-        data = dict.fromkeys(['ttype', 'timestamp', 'fromAddr', 'toAddr', 'pkey', 'value', 'fee', 'msg'])
+    def __init__(self, ttype : TTypes, fromAddr : Address, toAddr : Address, pkey : [str, PublicKey], value : int, fee : int, ckey : [None, PrivateKey] = None,  msg : [None, object] = None, datastring : [None, str] = None, timestamp : [None, datetime] = None, signature : [None, str] = None) -> None:
+        self.data = dict.fromkeys(['ttype', 'timestamp', 'fromAddr', 'toAddr', 'pkey', 'value', 'fee', 'msg'])
         self.data['ttype'] = ttype
-        self.data['timestamp'] = datetime.now()
+        self.data['timestamp'] = timestamp or datetime.now()
         self.data['fromAddr'] = fromAddr
         self.data['toAddr'] = toAddr
-        self.data['pkey'] = pkey.toCompressed()
+        if(isinstance(pkey, PublicKey)):
+            self.data['pkey'] = pkey.toCompressed()
+        else:
+            self.data['pkey'] = pkey
         self.data['value'] = value
         self.data['fee'] = fee
         self.data['msg'] = msg
         
-        self.datastring = dumps(self.data)
-        self.signature = Ecdsa.sign(self.datastring, ckey).toDer()
-        if(not self.validate(self.dataString, self.signature)):
+        self._datastring = self.encodeDatastring(self.data)
+        if(datastring and datastring != self._datastring):
+            raise Exception("Datastrings from created and imported transactions don't match")
+        self._signature = signature or Ecdsa.sign(self._datastring, ckey).toBase64()
+        if(not self.validate(self._datastring, self._signature)):
                 raise ValueError("Validation of a transaction has failed")
-
-    def __init__(self, dataString : str, signature : str) -> None:
-        self.dataString = dataString
-        self.signature = signature
-        if(not self.validate(self.dataString, Signature.fromDer(self.signature))):
-                raise ValueError("Validation of a transaction has failed")
-        data = dict.fromkeys(['ttype', 'timestamp', 'fromAddr', 'toAddr', 'pk', 'value', 'fee', 'msg'])
-        self.data = loads(self.dataString)
     
-    @staticmethod
-    def validate(dataString : str, signature : str) -> bool:
+    @property
+    def datastring(self) -> str:
+        return self._datastring
+
+    @property
+    def signature(self) -> str:
+        return self._signature
+    
+    @classmethod
+    def fromDatastring(cls, datastring : str, signature : str):
+        data = cls.decodeDatastring(datastring)
+        return cls(**data, datastring = datastring, signature = signature)
+    
+    @classmethod
+    def validate(cls, datastring : str, signature : str) -> bool:
         ret = True
-        data = loads(dataString)
-        if(not Ecdsa.verify(dataString, signature, PublicKey.fromCompressed(data['pkey']))):
+        data = cls.decodeDatastring(datastring)
+        if(not Ecdsa.verify(datastring, Signature.fromBase64(signature), PublicKey.fromCompressed(data['pkey']))):
             ret = False
             raise ValueError("Transaction didn't pass signature validation")
-        if(not (data.fromAddr[-40:] == data['pkey'][-40:])):
+        if(not (data["fromAddr"].getAddr()[-40:] == data['pkey'][-40:])):
             ret = False
             raise ValueError("Transaction sender don't own public key")
+        return ret
+
+    @staticmethod
+    def encodeDatastring(data : dict) -> str:
+        ret = dumps(data)
+        ret = base64.b64encode(ret).decode('ascii')
+        return ret
+
+    @staticmethod
+    def decodeDatastring(datastring : str) -> dict:
+        ret = base64.b64decode(datastring)
+        ret = loads(ret)
         return ret
 
