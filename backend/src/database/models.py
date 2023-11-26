a9935417ae784d84989c309fd60a71619135172b
Yury Lemetyuynen <altnightfall@yandex.ru>
2023-11-26 16:39:38 +0300

    changed tests

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -5,3 +5,5 @@ class Users(models.Model):
     username = fields.CharField(max_length=20, unique=True)
     full_name = fields.CharField(max_length=50, null=True)
     password = fields.CharField(max_length=128, null=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    modified_at = fields.DatetimeField(auto_now=True)
