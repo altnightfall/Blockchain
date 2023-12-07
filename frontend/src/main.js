e6c3adf7fd9bf3b47386d4e594fb883ddc6bca9a
Yury Lemetyuynen <altnightfall@yandex.ru>
2023-12-07 15:51:11 +0300

    Added simple front with table example

---
+++ frontend/src/main.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import { createApp } from "vue";
+import axios from 'axios';
+
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+
+axios.defaults.baseURL = 'http://localhost:5000/';  // the FastAPI backend
+
+app.use(router);
+app.mount("#app");
+
+import 'bootstrap';
