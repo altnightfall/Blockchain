8c30cd6e73d9221f9583ce9aaf1567b99c03c118
Yury Lemetyuynen <altnightfall@yandex.ru>
2023-12-07 22:50:45 +0300

    Vue

--- frontend/src/views/BlockbaseView.vue
+++ frontend/src/views/BlockbaseView.vue
@@ -1,5 +1,47 @@
 <template>
-  <div class="about" >
-    <h1>This is a blockbase page</h1>
+  <div class="blockbase">
+    <div class="text-center">
+      <button class="btn btn-light" v-on:click="showTable">Обновить</button>
+    </div>
+    <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">prevHash</th>
+        <th scope="col">nonce</th>
+        <th scope="col">datastring</th>
+        <th scope="col">hash</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="block in blocks" :key="block.id">
+        <td>{{block.id}}</td>
+        <td>{{block.prevHash}}</td>
+        <td>{{block.nonce}}</td>
+        <td>{{block.datastring}}</td>
+        <td>{{block.hash}}</td>
+      </tr>
+    </tbody>
+  </table>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+  data: () => ({ blocks: [] }),
+  methods: {
+    showTable(){
+      axios
+          .get('/block')
+          .then(response => {
+            this.blocks = response.data
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+    }
+  }
+}
+
+</script>
--- frontend/src/views/TransactionsView.vue
+++ frontend/src/views/TransactionsView.vue
@@ -1,5 +1,57 @@
 <template>
-  <div class="about" >
-    <h1>This is a transactions page</h1>
+  <div class="blockbase">
+    <div class="text-center">
+      <button class="btn btn-light" v-on:click="showTable">Обновить</button>
+    </div>
+    <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">parentBlockID</th>
+        <th scope="col">msg</th>
+        <th scope="col">data</th>
+        <th scope="col">fee</th>
+        <th scope="col">value</th>
+        <th scope="col">fromAddr</th>
+        <th scope="col">toAddr</th>
+        <th scope="col">timestamp</th>
+        <th scope="col">ttype</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="transaction in transactions" :key="transaction.id">
+        <td>{{transaction.id}}</td>
+        <td>{{transaction.parentBlockID}}</td>
+        <td>{{transaction.msg}}</td>
+        <td>{{transaction.data}}</td>
+        <td>{{transaction.fee}}</td>
+        <td>{{transaction.value}}</td>
+        <td>{{transaction.fromAddr}}</td>
+        <td>{{transaction.toAddr}}</td>
+        <td>{{transaction.timestamp}}</td>
+        <td>{{transaction.ttype}}</td>
+      </tr>
+    </tbody>
+  </table>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+  data: () => ({ transactions: [] }),
+  methods: {
+    showTable(){
+      axios
+          .get('/transaction')
+          .then(response => {
+            this.transactions = response.data
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+    }
+  }
+}
+
+</script>
