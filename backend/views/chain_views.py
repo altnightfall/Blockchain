2ca391b9d6252858ad248c968449f043498958de
funnemonkee <ghostsmans@gmail.com>
2023-12-22 04:21:47 +0300

    BISMILLAH

--- backend/views/chain_views.py
+++ backend/views/chain_views.py
@@ -55,14 +55,57 @@ async def mine_new_block(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"Unexpected error while validating mined block: {e}",
         )
+    get_tr = []
+    for tr_obj in tr:
+        get_tr.append(
+            await crud.get_transaction_by_id(session=session, transaction_id=tr_obj.id)
+        )
     block_dict: dict = mining_block.data.copy()
     block_dict.pop("id")
     block_dict["hash"] = mining_block.hash
     block_dict["datastring"] = mining_block.datastring
-    block_dict["transactionList"] = [tr.id for tr in tr]
+    block_dict["transactionList"] = [tr.id for tr in get_tr]
     block_to_create = BlockCreate(**block_dict)
-    await crud.create_block(session=session, block_inp=block_to_create)
-    return block_to_create
+    new_sesh = db_helper.get_scoped_session()
+    result = await crud.create_block(session=new_sesh, block_inp=block_to_create)
+    tr_temp = []
+    for tr in result.transactionList:
+        if tr.fromAddr:
+            from_addr = await crud.get_address_by_id(
+                session=new_sesh, address_id=tr.fromAddr
+            )
+            from_addr = AddressSchema(id=from_addr.id, address=from_addr.address)
+        else:
+            from_addr = None
+        if tr.toAddr:
+            to_addr = await crud.get_address_by_id(
+                session=new_sesh, address_id=tr.fromAddr
+            )
+            to_addr = AddressSchema(id=to_addr.id, address=to_addr.address)
+        else:
+            to_addr = None
+        tr_temp.append(
+            TransactionSchema(
+                ttype=tr.ttype,
+                ttimestamp=tr.ttimestamp,
+                fromAddr=from_addr,
+                toAddr=to_addr,
+                value=tr.value,
+                fee=tr.fee,
+                data=tr.data,
+                id=tr.id,
+                signature=tr.signature,
+                block_id=tr.block_id,
+            )
+        )
+    return Block(
+        prevHash=result.prevHash,
+        nonce=result.nonce,
+        datastring=result.datastring,
+        hash=result.hash,
+        id=result.id,
+        transactionList=tr_temp,
+    )
 
 
 @router.get("/chain_size/")
